9d5214bfbdc0138472199e5da4181a98900a958b
Vic678 <84173599+Vic678@users.noreply.github.com>
2023-09-19 19:04:13 +0530

    Initial commit

---
+++ 4-GuessTheNumber/chaiaurcode.js
@@ -0,0 +1,64 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+const p = document.createElement('p');
+let prevGuess = [];
+let numGuess = 1;
+let playGame = true;
+if (playGame) {
+  submit.addEventListener('click', function (e) {
+    e.preventDefault();
+    const guess = parseInt(userInput.value);
+    console.log(guess);
+    validateGuess(guess);
+  });
+}
+
+function validateGuess(guess) {
+  if (isNaN(guess)) {
+    alert('Please enter a valid number');
+  } else if (guess < 1) {
+    alert('Please enter a number greater than 1');
+  } else if (guess > 100) {
+    alert('Please enter a number samller than 1');
+  } else {
+    prevGuess.push(guess);
+    if (numGuess === 11) {
+      displayGuess(guess);
+      displayMessage(`Game Over . Random was ${randomNumber}`);
+      endGame();
+    } else {
+      displayGuess(guess);
+      checkGuess(guess);
+    }
+  }
+}
+function checkGuess(guess) {
+  if (guess === randomNumber) {
+    displayMessage(`Hurre , You guessed it right`);
+    endGame();
+  } else if (guess < randomNumber) {
+    displayMessage(`Please enter bigger number`);
+  } else if (guess > randomNumber) {
+    displayMessage(`Please enter smaller number`);
+  }
+}
+function displayGuess(guess) {
+  userInput.value = '';
+  guessSlot.innerHTML += `${guess}  `;
+  numGuess++;
+  remaining.innerHTML = `${11 - numGuess}`;
+}
+
+function displayMessage(guess) {
+  lowOrHi.innerHTML = `<h2>${message}</h2>`;
+}
+function newGame() {}
+function endGame() {}
+
+// console.log(randomNumber);
---
+++ README.md
@@ -0,0 +1,3 @@
+# dom-project-chaiaurcode-j2dtha
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/dom-project-chaiaurcode-j2dtha)
